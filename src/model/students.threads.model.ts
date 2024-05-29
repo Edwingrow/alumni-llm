478f26fd4c57e972d80a3ddab35541079d1d2614
Edwin García <edwingrow16@gmail.com>
2024-05-29 12:16:05 -0300

    quitar el json y dejar con memoria virtual

--- src/model/students.threads.model.ts
+++ src/model/students.threads.model.ts
@@ -2,22 +2,13 @@ import fs from 'fs';
 import path from 'path';
 
 
-const db = path.join(__dirname, '../db/conversationThreads.json');
-
+let threads: { [key: string]: any } = {};
 
 export const getThread = async (phone: string) => {
-    const data = fs.readFileSync(db, 'utf8'); 
-   return data ? JSON.parse(data)[phone] : null;
+    return threads[phone] || null;
 }
 
 
 export const saveThread = async (phone: string, threadId: any) => {
-    const data = fs.readFileSync(db, 'utf8');
-    let threads: { [key: string]: any } = {}; 
-    if (data) {
-        threads = JSON.parse(data);
-    }
-    threads[phone] = threadId.id
-    const newData = JSON.stringify(threads);
-    fs.writeFileSync(db, newData, 'utf8');
+    threads[phone] = threadId.id;
 }
